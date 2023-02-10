b6c81a2bdf0953865c7f1218235217e8c4620867
ericgo6969 <thiennbt.work@gmail.com>
2023-02-10 20:25:56 +0700

    [update] eslint

--- pages/api/imageproxy.ts
+++ pages/api/imageproxy.ts
@@ -1,7 +1,7 @@
 const async = async (req: any, res: any) => {
     const url = decodeURIComponent(req.query.url);
     const result = await fetch(url);
-    const body: any = result.body;
+    const { body }: any = result;
     body.pipe(res);
 };
 
